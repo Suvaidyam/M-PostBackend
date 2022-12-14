e743a06eb1657eac811a50283f20564d7c30813b
rahulsuvaidyam <rahul.suvaidyam@gmail.com>
2022-12-14 10:58:43 +0530

    delete modal

--- Api/Collection/index.js
+++ Api/Collection/index.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const crud = require('./Crud');
 router.get('/', crud.getCollection);
 router.post('/', crud.postCollection);
-// router.put('/:_id', crud.updateOne);
+router.put('/:_id', crud.putCollection);
+router.delete('/:_id', crud.deleteCollection);
 
 module.exports = router
