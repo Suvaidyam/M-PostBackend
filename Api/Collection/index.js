066ebf57c727d81551f0bbac7a1218f8b974f5ed
Abhishek Yadav <abhi.suvaidyam@gmail.com>
2023-10-19 11:11:49 +0530

    Add

--- Api/Collection/index.js
+++ Api/Collection/index.js
@@ -2,8 +2,8 @@ const express = require('express');
 const router = express.Router();
 
 const crud = require('./crud');
-// router.get('/:workspace_id', crud.getCollection);
-router.get('/', crud.getCollection);
+router.get('/:workspace_id', crud.getCollection);
+// router.get('/', crud.getCollection);
 router.post('/', crud.postCollection);
 router.put('/:_id', crud.putCollection);
 router.delete('/:_id', crud.deleteCollection);
