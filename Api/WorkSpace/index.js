bde7524f8e972eace033573caee47f6d7a34cf13
Abhishek Yadav <abhi.suvaidyam@gmail.com>
2023-11-22 10:45:35 +0530

    Add soft delete api

--- Api/WorkSpace/index.js
+++ Api/WorkSpace/index.js
@@ -5,6 +5,7 @@ const crud = require('./crud');
 router.get('/allWorkSpace', crud.findAllWorkSpace);
 router.get('/', crud.findWorkSpace);
 router.put('/:_id', crud.putWorkSpace);
+router.put('/softPutWorkSpace/:_id', crud.softPutWorkSpace);
 router.post('/', crud.postWorkSpace);
 router.delete('/:_id', crud.deleteWorkSpace);
 
